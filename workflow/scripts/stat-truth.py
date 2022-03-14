17a240b0a12a414ddaf386cd943d5f9a8afef2eb
Johannes Köster <johannes.koester@tu-dortmund.de>
2022-03-14 17:56:08 +0100

    fix

--- workflow/scripts/stat-truth.py
+++ workflow/scripts/stat-truth.py
@@ -2,7 +2,7 @@ import json
 from pysam import VariantFile
 
 
-with VariantFile(snakemake.input[0]) as infile, open(
+with VariantFile(open(snakemake.input[0], "rb") as infile, open(
     snakemake.output[0], "w"
 ) as outfile:
     json.dump({"isempty": not any(infile)}, outfile)
