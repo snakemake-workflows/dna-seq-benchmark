efa4b7bc3f168c9b02d3e93bf49f3652eeb2445d
Johannes Koester <johannes.koester@uk-essen.de>
2023-01-10 15:18:04 +0000

    fix: sort stratifications

--- workflow/scripts/collect-precision-recall.py
+++ workflow/scripts/collect-precision-recall.py
@@ -14,6 +14,19 @@ results = pd.concat(
     ],
     axis="rows",
 )
-results.sort_values("callset", inplace=True)
+
+def sort_key(col):
+    if col.name == "callset":
+        return col
+    else:
+        def cov_key(cov_label):
+            # return lower bound as integer for sorting
+            if ".." in cov_label:
+                return int(cov_label.split("..")[0])
+            else:
+                return int(cov_label[1:])
+        return col.apply(cov_key)
+
+results.sort_values(["callset", "coverage"], inplace=True, key=sort_key)
 
 results.to_csv(snakemake.output[0], sep="\t", index=False)
