faedced95520b9fdd4c2dbcebd8bac4dfacac9f9
Johannes Köster <johannes.koester@uni-due.de>
2023-07-07 14:02:38 +0200

    dbg

--- workflow/scripts/collect-fp-fn.py
+++ workflow/scripts/collect-fp-fn.py
@@ -12,7 +12,9 @@ import numpy as np
 
 
 def collect_chromosomes(f):
-    return pd.read_csv(f, sep="\t", dtype=str, usecols=["chromosome"]).drop_duplicates()["chromosome"].tolist()
+    d = pd.read_csv(f, sep="\t", dtype=str, usecols=["chromosome"]).drop_duplicates()
+    print(d)
+    return d["chromosome"].tolist()
 
 
 def read_data(f, callset, chromosome):
