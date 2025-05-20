b415c7cac46b6dfde35cde5c5c9b4601ec1f539f
Johannes Koester <johannes.koester@uni-due.de>
2025-05-20 09:59:13 +0200

    fix: use buffered and script based get-reads implementation

---
+++ workflow/scripts/get-reads.py
@@ -0,0 +1,39 @@
+import sys
+
+sys.stderr = open(snakemake.log[0], "w")
+
+import pysam
+import dnaio
+
+
+def aln_to_fq(aln):
+    return dnaio.SequenceRecord(
+        name=aln.query_name,
+        sequence=aln.get_forward_sequence(),
+        quality=aln.query_qualities,
+    )
+
+
+limit = snakemake.params.limit
+bam = pysam.AlignmentFile(snakemake.params.bam_url)
+
+buffer = {}
+n_written = 0
+with dnaio.open(snakemake.output[0], snakemake.output[1], "w") as fqwriter:
+    for aln in bam:
+        if n_written >= limit:
+            break
+        if not aln.is_secondary or aln.is_supplementary:
+            continue
+
+        qname = aln.query_name.removesuffix("/1").removesuffix("/2")
+
+        mate_aln = buffer.get(aln.query_name)
+        if mate_aln is None:
+            buffer[aln.query_name] = aln
+        else:
+            if aln.is_read2:
+                aln, mate_aln = mate_aln, aln
+
+            fqwriter.write(aln_to_fq(aln), aln_to_fq(mate_aln))
+            n_written += 1
