03e31b21b99c380145b22b706d3ff8fdc0dfdd69
Famke Bäuerle <famke.baeuerle@gmail.com>
2026-03-05 16:25:57 +0100

    change filtering

--- workflow/scripts/reformat-fp-fn-tp-tables.py
+++ workflow/scripts/reformat-fp-fn-tp-tables.py
@@ -5,6 +5,15 @@ sys.stderr = open(snakemake.log[0], "w")
 import pandas as pd
 
 df = pd.read_csv(snakemake.input.table, sep="\t")
+
+# Remove all normal samples
+if "SAMPLE" in df.columns:
+    if df["SAMPLE"].nunique(dropna=False) == 1:
+        pass
+    else:
+        df = df[df["SAMPLE"] == snakemake.params.tumor_sample_name]
+
+# Rename columns with the expression handed to the script
 df = df.rename(columns=snakemake.params.expression)
 
 # If vaf column contains per-sample dicts (from FORMAT fields),
@@ -16,7 +25,5 @@ if "vaf" in df.columns:
         if isinstance(v, str) and v.startswith("{")
         else v
     )
-
-df = df[df["SAMPLE"] == snakemake.params.tumor_sample_name]
 
 df.to_csv(snakemake.output.renamed_table, sep="\t", index=False)
